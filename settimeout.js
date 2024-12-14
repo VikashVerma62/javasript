0f83bbb4a09e62144615e3ac7135702692f29c40
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-14 08:24:07 -0800

    update

---
+++ settimeout.js
@@ -0,0 +1,20 @@
+let go=()=>{
+    let show=document.querySelector("#text")
+    
+    // setTimeout(()=>{
+    //     // show.innerHTML="hello cybrom";
+    //     show.innerHTML="";
+    // },3000)
+
+let date=new Date();
+let hour=date.getHours()
+let min=date.getMinutes()
+let sec=date.getSeconds()
+
+
+show.innerHTML=`${hour} : ${minit}: ${second}`;
+
+}
+
+
+
